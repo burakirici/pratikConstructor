871ecc26c897b5427fde29b66ebf6e1574f8eae7
BuraK Krc <burakbuci@hotmail.com>
2024-08-01 12:47:27 +0300

    Add project files.

---
+++ Bebek.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pratikConstructor
+{
+    public class Bebek
+    {
+        public string _name;
+        public string _surname;
+        public DateTime _Birthday;
+        public Bebek()
+        {
+            _name = "Emre";
+            _surname = "Terkan";
+            _Birthday = DateTime.Now;
+            Console.WriteLine("Ingaaaa" + " " + _Birthday);
+
+        }
+
+        public Bebek(string name, string surname) 
+        { 
+            _name = name;
+            _surname = surname;
+            _Birthday= DateTime.Now;
+            Console.WriteLine("Bebeğin adı: " + _name + " " + _surname, _Birthday);
+        }
+
+       
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using pratikConstructor;
+
+Bebek bebek = new Bebek();
+Console.WriteLine("Bebeğin Adı: " + bebek._name + " " + bebek._surname);
+Bebek bebek2 = new Bebek("Burak", " Kırıcı");
+Bebek bebek3 = new Bebek("Çağlayan", "Yılmaz");
+
